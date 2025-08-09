3ab963c1e21875a3a7a226ac1077e0e63cfa6b71
vanky0 <vanky0@users.noreply.github.com>
2025-08-09 00:00:00 +0000

    update project files and improvements

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -180,4 +180,9 @@ function drawVisualizer(bufferLength, dataArray) {
     
     // Request the next frame
     requestAnimationFrame(() => drawVisualizer(bufferLength, dataArray));
-    
+    
+    // Get the frequency data
+    analyser.getByteFrequencyData(dataArray);
+
+    const barWidth = (visualizerCanvas.width / bufferLength) * 2;
+    let x = 0;
